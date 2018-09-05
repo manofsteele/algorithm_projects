5e0c295c3407cd8eeff90e9d45ac628c56fbff7d
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-09-04 22:14:29 -0700

    Finish phase 1 of LRU Cache project

---
+++ firstname_lastname/lib/p01_int_set.rb
@@ -0,0 +1,109 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max)
+    @max = max
+  end
+
+  def insert(num)
+    @store[num] = true if is_valid?(num)
+  end
+
+  def remove(num)
+    @store[num] = false if is_valid?(num)
+  end
+
+  def include?(num)
+    if is_valid?(num) 
+      @store[num]
+    end
+  end
+
+  private
+
+  def is_valid?(num)
+    raise "Out of bounds" if num > @max || num < 0
+    true
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num].push(num) unless self.include?(num)
+  end
+
+  def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    resize! if @count == num_buckets    
+    self[num].push(num)
+    @count += 1
+  end
+
+  def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+      @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    new_store = Array.new(num_buckets * 2) {Array.new}
+    @store.each do |bucket| 
+      bucket.each do |el| 
+        new_store[el % (num_buckets * 2)].push(el)
+      end 
+    end 
+    @store = new_store
+  end
+end
