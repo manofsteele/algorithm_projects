6c2da898a1f7593bd23baee91b355ea5592c9245
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-09-04 22:39:59 -0700

    Finish phase 2 of LRU Cache project

--- firstname_lastname/lib/p02_hashing.rb
+++ firstname_lastname/lib/p02_hashing.rb
@@ -4,11 +4,18 @@ end
 
 class Array
   def hash
+    result = 0
+    self.each_with_index do |el, idx|
+      result += el.hash * idx.hash 
+    end 
+    result
   end
 end
 
 class String
   def hash
+    ords = self.chars.map { |el| el.ord}
+    ords.hash  
   end
 end
 
@@ -16,6 +23,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    result = 0 
+    self.each do |key, val| 
+      result += key.hash * val.hash
+    end 
+    result
   end
 end
